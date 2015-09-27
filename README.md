aad93aef3e8ffb8427acf3890dd3cfa7a552c242
slebden <slebden@gmail.com>
2015-09-27 12:57:59 +0200

    Assignment script and description / code book

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # GettingCleaningAssignment
-COurse Project dor Getting and Cleaning Data on Coursera
+Course Project for Getting and Cleaning Data on Coursera
+
+## The script
+The script is commented a bit over-the top, but to recap, here is what it does:
+- It reads test and training datasets
+- It reads the features and sets them as column names of read datasets
+- It reads the activity labels
+- It reads training and test activities
+- For both the training and test datasets (data frames) it adds an id column to the activities and merges them with the labels and sorts them on id. This is to assure that we will retain correspondence between subjects and activities
+- For both training and test datasets, a column is added with descriptive activity names
+- It reads training and test subjects and adds the corresponding column to test and training data frames
+-- From now on, information about subject and activity is present in both data sets along measurement data
+- It finally merges the test and training data frames into one data frames
+- It subsets the data to contain only needed columns: Activity and Subject and all measurements containing mean and stdev - columns which match mean() or std()
+- It melts the data on Activity and Subject Columns
+- It dcasts the molten data on Subject and Activity against variable calculating its mean
+- TADAAA. result variable contains what was required!
+
+## The output data set
+The output data set contains "mean of mean and std measurements for each subject and each activity". So the output data frame contains the following columns:
+- Subject - ranging from 1 to 30, identifying the Subject
+- Activity - decrtiptively labeled according to activity_labels.txt
+- 66 measurement columns
+
---
+++ run_analysis.R
@@ -0,0 +1,49 @@
+#Reading datasets
+train<-read.table("UCI\ HAR\ DataSet/train/X_train.txt", header = FALSE)
+test<-read.table("UCI\ HAR\ DataSet/test/X_test.txt", header = FALSE)
+#Adding an id column for later merging
+#train["id"]<-1:nrow(train)
+#test["id"]<-(nrow(train) + 1):(nrow(train) + nrow(test))
+#Reading features
+features<-read.table("UCI\ HAR\ DataSet/features.txt", header = FALSE)
+#Setting features as column names of datasets
+colnames(train)<-features[,2]
+colnames(test)<-features[,2]
+#Reading activity labels
+activity_labels<-read.table("UCI\ HAR\ DataSet/activity_labels.txt", header = FALSE)
+#Reading test activities
+ytest<-read.table("UCI\ HAR\ DataSet/test/y_test.txt", header = FALSE)
+#Adding an id column for later merging
+ytest["id"]<-1:nrow(ytest)
+#Merging activities and labels
+test_labeled<-merge(ytest, activity_labels)
+#Sorting by id to restore order
+test_labeled<-test_labeled[order(test_labeled$id),]
+#Adding labeled activity column to test data frame
+test[,"Activity"]<-test_labeled[,3]
+#Reading train activities
+ytrain<-read.table("UCI\ HAR\ DataSet/train/y_train.txt", header = FALSE)
+#Adding an id column for later merging
+ytrain["id"]<-1:nrow(ytrain)
+#Merging activities and labels
+train_labeled<-merge(ytrain, activity_labels)
+#Sorting by id to restore order
+train_labeled<-train_labeled[order(train_labeled$id),]
+#Adding labeled activity column to train data frame
+train[,"Activity"]<-train_labeled[,3]
+#Reading test subject
+subject_test<-read.table("UCI\ HAR\ DataSet/test/subject_test.txt", header = FALSE)
+#Adding subject column to test data frame
+test[,"Subject"]<-subject_test[,1]
+#Reading train subject
+subject_train<-read.table("UCI\ HAR\ DataSet/train/subject_train.txt", header = FALSE)
+#adding subject column to train data frame
+train[,"Subject"]<-subject_train[,1]
+#Merging data with 564 columns (561 original + Activity + Subject + id)
+alldata<-merge(test, train, all = TRUE, by = 1:563)
+#Cutting the data frame to contain only mean, std and our Activity and Subject
+alldata2<-alldata[,grepl("mean\\(\\)|std\\(\\)|Activity|Subject", colnames(alldata))]
+#Melting the data according to activity and subject
+molten<-melt(alldata2, id.vars=c("Activity", "Subject"))
+#Creating final dataset with means
+result<-dcast(molten, Subject + Activity ~ variable, mean)
